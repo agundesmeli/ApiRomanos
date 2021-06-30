479ae97ad78c4b1aa6982f2018256cfce93067b4
gundesamanda <amanda.gundes@mercadolivre.com>
2021-06-30 14:29:15 -0300

    Criação de API que transforma em números romanos

---
+++ src/main/java/br/com/meli/apiromanos/MeuController.java
@@ -0,0 +1,35 @@
+package br.com.meli.apiromanos;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MeuController {
+
+    @RequestMapping("/romanos/{numero}")
+    public String endPoint1(@PathVariable Integer numero) {
+        return converteRomano(numero);
+    }
+
+    private String converteRomano(Integer n) {
+        int[] vaNum = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] vaRom = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        String num = "";
+        int numero = n;
+
+        if (numero == 0) {
+            return "Não existe o 0 em romanos";
+        }
+        int i = 0;
+        while (numero > 0) {
+            if (numero >= vaNum[i]) {
+                num = num + vaRom[i];
+                numero -= vaNum[i];
+            } else {
+                i++;
+            }
+        }
+        return num;
+    }
+}
